8a78895fba7490f85f497b1e1daa9cb3e39f0f4e
Suvink <suvink456@yahoo.com>
2020-06-08 00:46:02 +0530

    Modified deploy script

--- config/deploy.php
+++ config/deploy.php
@@ -1,1 +1,12 @@
-<?php `git pull`;?>
+<php
+
+function execPrint($command){
+    $result = array();
+    exec($command, $result);
+    foreach($result as $line){
+        print($line."\n");
+    }
+}
+print("<pre>". execPrint("git pull")."</pre>");
+
+?>
