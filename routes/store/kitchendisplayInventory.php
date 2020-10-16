3175ef990e10292a24b47aa3156aa07b3399e4d9
AmdPathirana <64656686+AmdPathirana@users.noreply.github.com>
2020-10-16 12:53:11 +0530

    add me (#56)
    * added kitchenMagerDisplay
    
    * added kitchenMagerDisplay 2
    
    * added kitchenMagerDisplay 3
    
    * 2020-10-15-19:34

---
+++ routes/store/kitchendisplayInventory.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- Global Styles -->
+  <link rel="stylesheet" href="../../css/style.css" />
+  <!-- Local Styles -->
+  <link rel="stylesheet" href="../../css/kitchendisplay.css">
+  <title>kitchen Display</title>
+  <!-- <script type="text/javascript" src="../../js/kitchendisplay.js"></script> -->
+
+</head>
+
+<body>
+<!-- -----navi bar ---------- -->
+<div class="navbar">
+  <div class="columns group">
+    <div class="column is-2">
+      <img src="../../img/logo.png" height=56 width="224" />
+    </div>
+    <div class="column is-6 ml-5">
+      <nav class="nav">
+        <div class="nav-menu flex-row">
+          <ul class="nav-items "> 
+             <li class="nav-link ml-5 mr-5 zoom"  ><a href="kitchendisplayOrders">Orders</a></li>
+             <li class="nav-link  orange-color ml-5 zoom" ><a href="kitchendisplayInventory">Items</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <div class="column is-3 has-text-right nav-logout">
+      <i class="fa fa-user" aria-hidden="true"></i>
+      <span class="mr-1">Kitchen Manager</span>
+      <button class="button is-primary">Logout</button>
+    </div>
+  </div>
+</div>
+ 
+<!--------xx-----navi bar --------xx------->
+
+<!----------- Main section------------>
+
+  <section>
+      <div class="column is-12">           
+        <div class="tabs">
+          <input type="radio" id="tab1" name="tab-control" checked>
+          <input type="radio" id="tab2" name="tab-control">
+          <ul>
+            <li title="O_Orders"><label for="tab1" role="button"><br><span>Inventory</span></label></li>
+            <li title="D_Orders"><label for="tab2" role="button"><br><span>Retrieve Items</span></label></li>
+          </ul>
+
+          <div class="slider">
+            <div class="indicator"></div>
+          </div>
+          <div class="content">
+            <section>
+              <h2>Inventory</h2>
+             
+              <h1 class="orange-color mt-0 mb-1">Inventory here</h1>
+            </section>
+            <section>
+               <h2>Retreive Items</h2>
+              
+            <h1 class="orange-color mt-0 mb-1">Retrieve Items here</h1>
+          </div>
+        </div>
+      </div>
+     
+  </section>
+<!----xx------- Main section----xx-------->
+
+<!------------pop up orders-------------->
+<div class="popup" id="popup-1">
+  <div class="overlay"></div>
+  <div class="pop-content">
+    <div class="close-btn zoom" onclick="togglePopup()">&times;</div>
+     <div class="column is-12 ml-0 mr-0">
+        <div class="card">
+          <h2 class="orange-color mt-0 mb-1">Order 5667lH</h2>
+
+            <!------- show ordered list ----------->
+            <div class="order-selected-item">
+              <div class="order-selected-row">
+                <div class="order-selected-row-image">
+                  <img src="https://image.flaticon.com/icons/svg/184/184410.svg">
+                </div>
+                <div class="order-selected-row-description has-text-left">
+                  <h4 class="mb-0 mt-0">Chinese Ramen</h4>
+                </div>
+                <div class="order-selected-row-quantity">
+                  <h4 class="mb-0 mt-0">1</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!----XX--- show ordered list ------XX----->
+          <div class="total-box d-flex">
+            <div class="title-col">
+              <h3 class="mt-1 mb-1">Total Amount</h3>
+              <h3 class="mt-1 mb-1">Arived Time</h3>
+              <h3 class="mt-1 mb-1">Order Location</h3>
+            </div>
+            <div class="price-col has-text-right mr-1">
+            <h3 class="mt-1 mb-1">450.00</h3>
+            <h3 class="mt-1 mb-1">10:20 pm</h3>
+            <h3 class="mt-1 mb-1">Negomabo xxxxx  </h3>
+            </div>
+          </div>
+          <!------accept/decline/prepared/riders btn -------->
+          <div class="columns group">
+            <div class="column is-3">
+               <button class="button is-primary mt-1 zoom">Accept</button>
+            </div>
+            <div class="column is-3">
+              <button class="button is-link mt-1 zoom " onclick="togglePopup2()">Riders</button>
+           </div>
+            <div class="column is-3">
+              <button class="button is-success mt-1 zoom">Prepared</button>
+           </div>
+            <div class="column is-3">
+               <button class="button is-danger mt-1 zoom">Decline</button>
+            </div>
+          </div>
+           <!-----XX---accept/decline btn ---XX------>
+        </div>
+      </div>
+      
+  </div>
+</div>
+<!---------xx---pop up orders-----xx--------->
+
+<!----------pop up delivery people ----------------->
+<div class="popup" id="popup-2">
+  <div class="pop-content2">
+    <div class="close-btn" onclick="togglePopup2()">&times;</div><br><br>
+    <h2 class="orange-color mt-0 mb-1">Available Delevery People</h2>
+    <div class="menu-cards">
+      <div class="dp-card" onclick="togglePopup3()">
+        <div class="columns group">
+          <div class="column is-6">
+            <h3 class="mt-1 mb-0">Rieder 025R</h3>
+          </div>
+          <div class="column is-6">
+            <h3 class="mt-1 mb-0 rider-status">Free</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="menu-cards">
+      <div class="dp-card" onclick="togglePopup3()">
+        <div class="columns group">
+          <div class="column is-6">
+            <h3 class="mt-1 mb-0">Rider xxxx</h3>
+          </div>
+          <div class="column is-6">
+            <h3 class="mt-1 mb-0 rider-status">Free</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="menu-cards">
+      <div class="dp-card" onclick="togglePopup3()">
+        <div class="columns group">
+          <div class="column is-6">
+            <h3 class="mt-1 mb-0">Rider xxxx</h3>
+          </div>
+          <div class="column is-6">
+            <h3 class="mt-1 mb-0 rider-status">Free</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="menu-cards">
+      <div class="dp-card" onclick="togglePopup3()">
+        <div class="columns group">
+          <div class="column is-6">
+            <h3 class="mt-1 mb-0">Rider xxxx</h3>
+          </div>
+          <div class="column is-6">
+            <h3 class="mt-1 mb-0 rider-status">Free</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</div>
+<!------XX----pop up delivery people -------XX---------->
+
+<!----------pop up 03/ summery ----------------->
+<div class="popup" id="popup-3">
+  <div class="pop-content3">
+    <div class="close-btn" onclick="togglePopup3()">&times;</div><br><br>
+
+    <h2 class="orange-color mt-0 mb-1 ">Order Asign Summery</h2>
+    <div class="menu-cards">
+      <div class="asiggned-summery-card" >
+        <div class="columns group">
+          <div class="column is-6">
+            <h3 class="mt-1 mb-2">Order Id</h3>
+            <h3 class="mt-1 mb-2">Assigned Time</h3>
+            <h3 class="mt-1 mb-2">Location</h3>
+            <h3 class="mt-1 mb-2">________________________</h3>
+            <h3 class="mt-1 mb-2">Rider ID</h3>
+            <h3 class="mt-1 mb-2">Rider Name</h3>
+            <h3 class="mt-1 mb-2">Rider MOb Num</h3>
+          
+            
+          </div>
+          <div class="column is-6">
+            <h3 class="mt-1 mb-2 rider-status">___________</h3>
+            <h3 class="mt-1 mb-2 rider-status">___________</h3>
+            <h3 class="mt-1 mb-2 rider-status">___________</h3>
+            <h3 class="mt-1 mb-2 rider-status">___________</h3>
+            <h3 class="mt-1 mb-2 rider-status">___________</h3>
+            <h3 class="mt-1 mb-2 rider-status">___________</h3>
+            <h3 class="mt-1 mb-2 rider-status">___________</h3>
+          </div>
+          <div class="column is-3">
+            <button class="button is-primary ml-4 mt-1 zoom" onclick="kk()">Asigned Order</button>
+         </div>
+        </div>
+      </div>
+    </div>
+    
+    
+  </div>
+</div>
+<!------XX----pop up 03/summery -------XX---------->
+
+<!-- --------kitchen display js file -->
+<script type="text/javascript" src="../../js/kitchendisplay.js"></script>
+</body>
+
+</html>
