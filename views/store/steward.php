d498ef0c3777fc2c84fcf7ec87ac4b26d68b30ff
Hasantha96 <nphlakshan@gmail.com>
2021-03-27 23:21:18 +0530

    minor changes

--- views/store/steward.php
+++ views/store/steward.php
@@ -221,7 +221,7 @@ if (isset($_POST['logout'])) {
 				let responseData = JSON.parse(await response.text());
 				console.log(responseData);
 				if(responseData.status=='AVAILABLE'){
-					// document.querySelector("#switch")
+					document.querySelector("#switch").style
 					return '1';
 				}
 
