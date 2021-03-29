bb9010428a1b9c5eb2c6cb18cb2e814057c12b26
suvink <suvink456@yahoo.com>
2021-03-30 04:46:14 +0530

    Added backup services

--- services/backup.sh
+++ services/backup.sh
@@ -1,5 +1,5 @@
 TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
 echo "SQL DUMP STARTED AT ${TIMESTAMP}"
 touch "../backups/Backup-${TIMESTAMP}.sql"
-/Applications/XAMPP/bin/mysqldump --user=eatme --password=weM2KxUZriR9 --host=localhost eat_me > ../backups/Backup-${TIMESTAMP}.sql
+mysqldump --user=eatme --password=weM2KxUZriR9 --host=localhost eat_me > ../backups/Backup-${TIMESTAMP}.sql
 echo "BACKUP FINISHED"
